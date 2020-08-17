ceb96a9389eae69ea93c46a1da89ce77f130b8ab
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-08-17 15:38:11 +0200

    Implementation of the logic class User.

---
+++ sya/lib/logic/User.dart
@@ -0,0 +1,11 @@
+import 'package:sya/logic/WebSite.dart';
+
+/// Represent a user with the various useful information
+class User {
+  /// Password of the user.
+  static String password;
+  /// List of the different websites.
+  static List<WebSite> listOfWebSites;
+  /// Theme used by the user.
+  static String theme;
+}
