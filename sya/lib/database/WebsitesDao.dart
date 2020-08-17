e5a0cffcb2e3c20811c5e2d4cc03433a915d7042
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-08-17 19:25:34 +0200

    Added a new method for add a website i n the DB.

--- sya/lib/database/WebsitesDao.dart
+++ sya/lib/database/WebsitesDao.dart
@@ -5,16 +5,21 @@ import 'package:sya/logic/User.dart';
 /// Manage the data of the websites.
 class WebSitesDao {
   /// Return the content of the database.
-  Future<List<WebSite>> getWebSites() async {
+  static Future<List<Website>> getWebSites() async {
     List<Map<String, dynamic>> retQuery = await DaoMaster.query("WebSite", columns: ["rowid", "name", "login", "cryptedPassword"]);
-    List<WebSite> ret = new List<WebSite>();
+    List<Website> ret = new List<Website>();
 
     for (int i = 0; i < retQuery.length; i++)
-      ret.add(new WebSite(retQuery[i]['rowquery'], retQuery[i]['name'], retQuery[i]['login'], retQuery[i]['cryptedPassword']));
+      ret.add(new Website(retQuery[i]['rowquery'], retQuery[i]['name'], retQuery[i]['login'], retQuery[i]['cryptedPassword']));
 
     return ret;
   }
 
   /// Return the key.
-  Future<String> getTheKey() async => ((await getWebSites())[0]).decrypt(User.password);
+  static Future<String> getTheKey() async => ((await getWebSites())[0]).decrypt(User.password);
+
+  /// Add a website in the DB.
+  static Future addAWebsite(Website website) async {
+    await DaoMaster.insert("WebSite", {"name" : website.name, "login" : website.login, "cryptedPassword" : website.cryptedPassword});
+  }
 }
