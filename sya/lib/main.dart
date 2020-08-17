b35118af64ebe1019010b6fa1bd9ab0de3a46b77
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-08-17 20:27:26 +0200

    Test of the Website class and DAO.

--- sya/lib/main.dart
+++ sya/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sya/database/DaoMaster.dart';
+import 'package:sya/database/WebsitesDao.dart';
+import 'package:sya/logic/Website.dart';
 
 import 'database/MemoryDao.dart';
 
@@ -30,12 +33,16 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             RaisedButton(
-              child: Text("Change theme"),
-              onPressed: () async {await MemoryDao.changeTheme("light");},
+              child: Text("Add a website"),
+              onPressed: () async {await WebsitesDao.addAWebsite(new Website(1, "name", "login", "cryptedPassword"));},
             ),
             RaisedButton(
-              child: Text("Print theme"),
-              onPressed: () async {print(await MemoryDao.getTheme());},
+              child: Text("Get a website"),
+              onPressed: () async {print(await WebsitesDao.getWebsites());},
+            ),
+            RaisedButton(
+              child: Text("Delete the table"),
+              onPressed: () async {DaoMaster.delete("WebSite");},
             )
           ],
         ),
