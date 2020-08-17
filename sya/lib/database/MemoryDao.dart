abd1c1eb0c170b38edbf50519297c0df6f0f5816
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-08-17 19:14:38 +0200

    Added the method for change the theme.

--- sya/lib/database/MemoryDao.dart
+++ sya/lib/database/MemoryDao.dart
@@ -6,4 +6,8 @@ class MemoryDao {
   static Future<String> getTheme() async {
     return (await DaoMaster.query("Memory", columns: ['themeColor']))[0]['themeColor'];
   }
+
+  static Future<int> changeTheme(String theme) async {
+    return await DaoMaster.update("Memory", {"rowid" : 0, "themeColor" : theme}, where: "rowid = ?", whereArgs: [0]);
+  }
 }
