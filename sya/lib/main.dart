ca5bdbd9466939a639fa4624951ac2e83f1be4ca
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-08-22 20:11:21 +0200

    Test of InformationWindow.

--- sya/lib/main.dart
+++ sya/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sya/database/DaoMaster.dart';
 import 'package:sya/database/WebsitesDao.dart';
+import 'package:sya/ihm/Windows/InformationWindow.dart';
 import 'package:sya/ihm/Windows/MainWindow.dart';
 import 'package:sya/ihm/Windows/WelcomeWindow.dart';
 import 'package:sya/logic/Website.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       // home: MyHomePage(),
-      home: MainWindow(),
+      home: InformationWindow(),
       theme: ThemeData.dark(),
     );
   }
