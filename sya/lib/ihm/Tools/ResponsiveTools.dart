0e0457f99653c145b20d567f3324683262d7e72e
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-09-19 10:44:12 +0200

    Change of size.

--- sya/lib/ihm/Tools/ResponsiveTools.dart
+++ sya/lib/ihm/Tools/ResponsiveTools.dart
@@ -3,7 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 /// Outils pour la gestion du responsive dans l'application.
 class ResponsiveTools {
   /// Initialise ScreenUtils.
-  static void initScreenUtils(context) => ScreenUtil.init(context, width : 360, height : 640, allowFontScaling: true);
+  static void initScreenUtils(context) => ScreenUtil.init(context, width : 360, height : 780, allowFontScaling: true);
 
   /// Retourne la taille du texte.
   static double textSize(double size) => ScreenUtil().setSp(size);
