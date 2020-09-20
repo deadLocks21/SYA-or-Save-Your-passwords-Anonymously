f54958deb34bda589728fd4a580e2d63bcf78040
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-09-20 14:14:12 +0200

    Launch of the new UI.

--- sya/lib/main.dart
+++ sya/lib/main.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:sya/database/DaoMaster.dart';
-import 'package:sya/database/WebsitesDao.dart';
-import 'package:sya/ihm/Windows/InformationWindow.dart';
-import 'package:sya/ihm/Windows/MainWindow.dart';
-import 'package:sya/ihm/Windows/WebsiteNameWindow.dart';
-import 'package:sya/ihm/Windows/WelcomeWindow.dart';
-import 'package:sya/logic/Website.dart';
 
-import 'database/MemoryDao.dart';
-import 'ihm/Tools/ResponsiveTools.dart';
-import 'ihm/Windows/IntroductionIntentWindow.dart';
-import 'ihm/Windows/VerificationWindow.dart';
+import 'ihm/Windows/OpenWindow.dart';
 
 
 void main() => runApp(MyApp());
@@ -27,55 +17,10 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       // home: MyHomePage(),
-      // home: WelcomeWindow(),
+      home: OpenWindow(),
       // home: WebsitNameWindow(website: new Website(id: null),),
-      home: IntroductionIntentWindow(),
+      // home: IntroductionIntentWindow(),
       theme: ThemeData.dark(),
     );
   }
 }
-
-/*
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RaisedButton(
-              child: Text("Add a website"),
-              onPressed: () async {await (new Website(1, "name", "login", "cryptedPassword")).add();},
-            ),
-            RaisedButton(
-              child: Text("Get websites"),
-              onPressed: () async {print(await WebsitesDao.getWebsites());},
-            ),
-            RaisedButton(
-              child: Text("Modify a website"),
-              onPressed: () async {await (new Website(1, "name", "login", "crypted")).save();},
-            ),
-            RaisedButton(
-              child: Text("Delete the table"),
-              onPressed: () async {
-//DaoMaster.delete("WebSite");
-await DaoMaster.open();
-await DaoMaster.db.execute("DROP TABLE IF EXISTS Website");
-await DaoMaster.db.execute("CREATE TABLE IF NOT EXISTS Website(name TEXT, login TEXT, cryptedPassword TEXT)");
-await DaoMaster.close();
-              },
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
